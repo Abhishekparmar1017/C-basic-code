da2e8b528f4d95b2eb630e9336941a09c5cf351c
Abhishekparmar1017 <156321834+Abhishekparmar1017@users.noreply.github.com>
2024-02-28 16:28:58 +0530

    Add files via upload

---
+++ base.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//	cout<<"Hello";
+//int i=18;
+//if(i>18){
+//	cout<<"bad";
+//}
+//else if(i<=18){
+//	cout<<"not bad";
+//}
+//else{
+//	cout<<"good"; 
+//}
+  int i=0;
+while(i<11){
+	cout<<i<<endl;
+	i++;
+}
+//do{
+//	cout<<i<<endl;
+//	i++;
+//}
+//while(i<6);
+//for(int i=0;i<5; i++){
+//	cout<<i<<endl;
+//}
+	return 0;
+}
+
+//
+//#include<bits/stdc++.h>
+//using namespace std;
+//// function
+//void job() {
+//  cout << "You do hard work and this car..!";
+//}
+//int main(){
+////	calling function
+//		job();
+//		cout<<endl;
+////		Array
+//	string car[3]={"BMW","Aude","Ford"};
+//	cout<<car[2];
+//	return 0;
+//}
